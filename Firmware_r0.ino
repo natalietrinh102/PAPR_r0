de5b758d089e9746e5ce52d25a4b9da67f61ccc7
natalietrinh102 <34173043+natalietrinh102@users.noreply.github.com>
2020-04-30 21:37:46 -0700

    Update Firmware_r0.ino

--- Firmware_r0.ino
+++ Firmware_r0.ino
@@ -1,36 +1,114 @@
-int fanPin = 3;  // ATMEGA328 only supports full fast PWM on pin 3 (reduces motor whine)
-int switchLow = 7; //Prefer non-PWM pins for switch
-int switchHigh = 4;
+/*
+OPEN SOURCE PAPR
 
+Due to specfific changes to timer registers, this code is optimized to run on the ATMEGA328 platform only.
+
+ */
+
+#include <QC2Control.h>
+/*
+PWM Tuning
+This values provide PMW frequency tuning for pin 3. Play with this if the fan/motor whines excessively.
+Different fans/motors will prefer different frequencies, and some will whine regardless. 
+BIGGER IS NOT BETTER. Higher frequencies change motor whine frequency at the cost of torque and resolution.
+
+Note changing the frequency will mess with the manual mode calibration! 
+
+timer2Div |    PWM
+   value  | frequency
+-----------------------
+1         | 31372.55 Hz
+8         | 3921.16 Hz
+32        | 980.39 Hz
+64        | 490.20 Hz (The DEFAULT)
+128       | 245.10 Hz
+256       | 122.55 Hz
+1024      | 30.64 Hz
+*/
+int timer2Div = 64
+
+// Manual Override Mode
+bool manualEnabled = true; // true = disables feedback control. PAPR must be manually tuned by technician when changing between models of filters, batteries, or fans. 
+int lowFanmod = 0.8 // Multiplier for low fan speed (ex. lowFanmod = 0.8 -> 80% of max PWM duty cycle)
+int medFanmod = 0.9
+int highFanmod = 1
+
+// Quickcharge
+bool qcEnabled = true; // true = Re-establish handshake on heart, false = disable QC Heartbreak
+int qcDataPos = 2
+int qcDataNeg = 5
+QC2Control quickCharge(2, 5);
+
+// Fan Control
+int fanPin = 3;  // ATMEGA328 only supports full fast PWM on pin 3 (reduces motor whine)
+int switchLow = 7; // Prefer non-PWM pins for switch
+int switchHigh = 4;
 int fanSpeed = 255; // Integer fan speed is max by default
 int LowOn;
 int HighOn;
 int SpeedSet;
-int TargetCfm;
 
+// Flow Feedback
+int TargetCfm;
 int CurrentCfm;
 
 void setup() {
+// Start Debug interface
 Serial.begin(9600);
 
-/*
-This values provide PMW frequency tuning for pin 3. Play with this if the fan/motor whines excessively.
-Different fans/motors will prefer different frequencies, and some will whine regardless.
-Note changing the frequency will mess with the manual mode calibration! 
-*/
-//TCCR2B = TCCR2B & B11111000 | B00000001;    // set timer 2 divisor to     1 for PWM frequency of 31372.55 Hz
-//TCCR2B = TCCR2B & B11111000 | B00000010;    // set timer 2 divisor to     8 for PWM frequency of  3921.16 Hz
-//TCCR2B = TCCR2B & B11111000 | B00000011;    // set timer 2 divisor to    32 for PWM frequency of   980.39 Hz
-//TCCR2B = TCCR2B & B11111000 | B00000100;    // set timer 2 divisor to    64 for PWM frequency of   490.20 Hz (The DEFAULT)
-//TCCR2B = TCCR2B & B11111000 | B00000101;    // set timer 2 divisor to   128 for PWM frequency of   245.10 Hz
-//TCCR2B = TCCR2B & B11111000 | B00000110;    // set timer 2 divisor to   256 for PWM frequency of   122.55 Hz
-//TCCR2B = TCCR2B & B11111000 | B00000111;    // set timer 2 divisor to  1024 for PWM frequency of    30.64 Hz
+// Adjust PWM as needed
+switch (timer2Div) {
+  case 1:
+    Serial.println("PWM Frequency set to 31372.55 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000001;
+    break;
+  case 8:
+    Serial.println("PWM Frequency set to 3921.16 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000010;
+    break;
+  case 32:
+    Serial.println("PWM Frequency set to 980.39 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000011;
+    break;
+  case 64:
+    Serial.println("PWM Frequency set to 420.20 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000100;
+    break;
+  case 128:
+    Serial.println("PWM Frequency set to 245.10 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000101;
+    break;
+  case 256:
+    Serial.println("PWM Frequency set to 122.55 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000110;
+    break;
+  case 1024:
+    Serial.println("PWM Frequency set to 30.64 Hz");
+    TCCR2B = TCCR2B & B11111000 | B00000111;
+    break;
+  default:
+    Serial.println("ERROR: Invalid PWM Frequency requested. Frequency not changed.");
+    break;
+}
 
+// Setup fan control
 pinMode(fanPin,OUTPUT);
 pinMode(switchLow,INPUT_PULLUP);
 pinMode(switchHigh,INPUT_PULLUP);
 
-// Allow the fan to spool up before continuing - this helps prevent stalls
+// Initial Quickcharge handshake
+// Note - QC needs a very short delay before accepting handshakes - run setVoltage() late in setup
+switch (qcEnabled) {
+  case true:
+    Serial.println("Sending Initial Quickcharge handshake");
+    quickCharge.setVoltage(12);
+    break;
+  case false:
+    Serial.println("Quickcharge disabled in Settings, bypassing initial handshake");
+    break;
+}
+
+// Spool fan up before continuing - this helps prevent stalls
 Serial.println("Spooling fan");
 analogWrite(fanPin, 255);
 delay(1000);
@@ -38,6 +116,9 @@ delay(1000);
 
 void loop() {
 
+
+
+// Monitor switch state (move this to interupt later)
   Serial.println("Attempting to read switch state...");
 // Read switch and determine perferred airflow.
   LowOn = digitalRead(switchLow);
@@ -66,7 +147,15 @@ void loop() {
 CurrentCfm = TargetCfm; // Bypass the CFM calculations
 
 // Alternative manual mode only
-switch (SpeedSet) {
+manualMode(SpeedSet);
+
+
+// Push fan speed to fan
+analogWrite(fanPin, fanSpeed);
+}
+
+void manualMode(SpeedSet){
+  switch (SpeedSet) {
   case 1:
     Serial.println("Manually setting speed to 80%");
     fanSpeed = .80 * 255;
@@ -83,8 +172,4 @@ switch (SpeedSet) {
     Serial.println("ERROR: SpeedSet is invalid!");
     break;
 }
-
-
-// Push fan speed to fan
-analogWrite(fanPin, fanSpeed);
 }
